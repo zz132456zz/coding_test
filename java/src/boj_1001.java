562aef4ef74d0ac51c9ea9e5a7898a539e1bac37
zz132456zz <zz132456zz@kakao.com>
2021-11-25 21:24:06 +0900

    211125 boj

---
+++ java/src/boj_1001.java
@@ -0,0 +1,14 @@
+// 백준 1001 - A-B
+
+import java.util.Scanner;
+
+public class boj_1001 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A - B);
+    }
+
+}
