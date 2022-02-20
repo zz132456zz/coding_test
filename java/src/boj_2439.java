62fbe345ef9b4b79d20d0b4a82b7a91100fa6217
이도경 <zz132456zz@kakao.com>
2022-02-20 18:59:04 +0900

    220220 boj

---
+++ java/src/boj_2439.java
@@ -0,0 +1,27 @@
+// 백준 2439 - 별 찍기 - 2
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2439 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+
+}
