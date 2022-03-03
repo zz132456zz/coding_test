84b87f12e034f63030af707110224e8afbce1ed3
이도경 <zz132456zz@kakao.com>
2022-03-03 21:35:33 +0900

    220303 boj

---
+++ java/src/boj_10172.java
@@ -0,0 +1,17 @@
+// 백준 10172 - 개
+
+public class boj_10172 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("|\\_/|\n");
+        sb.append("|q p|   /}\n");
+        sb.append("( 0 )\"\"\"\\\n");
+        sb.append("|\"^\"`    |\n");
+        sb.append("||_/=\\\\__|\n");
+
+        System.out.println(sb);
+    }
+
+}
