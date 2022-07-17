a5890d49929d06977ea26f5c4a4725c66f04f6e9
이도경 <zz132456zz@kakao.com>
2022-07-17 13:04:54 +0900

    220717 boj

---
+++ java/src/boj_24484.java
@@ -0,0 +1,71 @@
+// 백준 24484 - 알고리즘 수업 - 깊이 우선 탐색 6
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class boj_24484 {
+
+    static int N;
+    static int cnt;
+    static long[] depth;
+    static long[] order;
+    static ArrayList<ArrayList<Integer>> data;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        depth = new long[N + 1];
+        order = new long[N + 1];
+        Arrays.fill(depth, -1);
+        data = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            data.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            data.get(u).add(v);
+            data.get(v).add(u);
+        }
+
+        for (int i = 0; i <= N; i++) {
+            Collections.sort(data.get(i), Collections.reverseOrder());
+        }
+
+        visited = new boolean[N + 1];
+        cnt = 1;
+
+        dfs(R, 0);
+
+        long answer = 0;
+        for (int i = 1; i <= N; i++) {
+            answer += order[i] * depth[i];
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void dfs(int start, int d) {
+        visited[start] = true;
+        depth[start] = d;
+        order[start] = cnt++;
+
+        for (int idx : data.get(start)) {
+            if (!visited[idx]) dfs(idx, d + 1);
+        }
+    }
+
+}
