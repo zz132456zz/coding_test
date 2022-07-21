df393824cff173f856b433ae7216e700c9eade54
이도경 <zz132456zz@kakao.com>
2022-07-21 23:25:02 +0900

    220721 boj

---
+++ java/src/boj_10926.java
@@ -0,0 +1,16 @@
+// 백준 10926 - ??!
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_10926 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder(br.readLine());
+        System.out.println(sb.append("??!"));
+    }
+
+}
